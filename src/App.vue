8ad3bc4b82ef6e6f6af80f57f1d490de8cd23593
Seth Zerwas <zerw2167@stthomas.edu>
2022-12-17 21:00:29 -0600

    Added Legend (Table needs legend colors still)

--- src/App.vue
+++ src/App.vue
@@ -346,6 +346,22 @@ export default {
                 <FilterForm @update-rows="updateTable" />
             </div>
         </div>
+        <div class="grid-container">
+            <div class="grid-x grid-padding-x">
+                <div class='my-legend'>
+                    <div class='legend-title'>Different Crime Categories</div>
+                    <div class='legend-scale'>
+                        <ul class='legend-labels'>
+                            <li><span style='background:#8DD3C7;'></span>Crimes Against a Person</li>
+                            <li><span style='background:#FFFFB3;'></span>Crimes Against Property</li>
+                            <li><span style='background:#BEBADA;'></span>Inchoate Crimes</li>
+                            <li><span style='background:#FB8072;'></span>Statutory Crimes</li>
+                            <li><span style='background:#80B1D3;'></span>Other Crimes</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
         <div>
             <table class="table">
                 <thead>
@@ -403,4 +419,42 @@ export default {
     text-align: center;
     cursor: pointer;
 }
+
+.my-legend .legend-title {
+    text-align: left;
+    margin-bottom: 5px;
+    font-weight: bold;
+    font-size: 90%;
+    }
+  .my-legend .legend-scale ul {
+    margin: 0;
+    margin-bottom: 5px;
+    padding: 0;
+    float: left;
+    list-style: none;
+    }
+  .my-legend .legend-scale ul li {
+    font-size: 80%;
+    list-style: none;
+    margin-left: 0;
+    line-height: 18px;
+    margin-bottom: 2px;
+    }
+  .my-legend ul.legend-labels li span {
+    display: block;
+    float: left;
+    height: 16px;
+    width: 30px;
+    margin-right: 5px;
+    margin-left: 0;
+    border: 1px solid #999;
+    }
+  .my-legend .legend-source {
+    font-size: 70%;
+    color: #999;
+    clear: both;
+    }
+  .my-legend a {
+    color: #777;
+    }
 </style>
