8b8e0a9d223bd8afa92b1e5f50619b49afc58798
Ani <rajani.lamichhane7@gmail.com>
2022-12-18 05:38:09 -0600

    makers

--- src/App.vue
+++ src/App.vue
@@ -22,6 +22,7 @@ export default {
             filter: false,
             currFilter: null,
             layerGroup: null,
+            visibleMarkers: [],
             leaflet: {
                 map: null,
                 center: {
@@ -52,7 +53,8 @@ export default {
                     { location: [44.913106, -93.170779], marker: 'Highland' },
                     { location: [44.937705, -93.136997], marker: 'Summit Hill' },
                     { location: [44.949203, -93.093739], marker: 'Downtown' }
-                ]
+                ],
+                icons: {}
             },
         };
     },
@@ -74,15 +76,16 @@ export default {
             if (!el.value) return;
             if (el.value.split(',').length == 2) {
                 let location = el.value.split(',');
-                if((44.883658 <= parseFloat(location[0]) <= 45.008206) && ((-93.217977 <= parseFloat(location[1])) && (parseFloat(location[1]) <= -92.993787))){
+                if ((44.883658 <= parseFloat(location[0]) <= 45.008206) && ((-93.217977 <= parseFloat(location[1])) && (parseFloat(location[1]) <= -92.993787))) {
                     this.leaflet.map.flyTo(el.value.split(','), 14);
                 } else {
                     alert("Please enter coordinates within the St. Paul city boundaries!");
                 }
             } else {
                 let loc = el.value.split(" ");
-                loc[0].replace('X', '0');
+                loc[0] = loc[0].replace(/X/g, '0');
                 loc.push("MN");
+                loc.push("SAINT PAUL");
                 loc = loc.join(" ");
                 this.getText(`https://nominatim.openstreetmap.org/search?q=${loc}&format=jsonv2`)
                     .then(res => {
@@ -116,10 +119,10 @@ export default {
         onMapClick(e) {
             this.updateLocation(null, e.latlng);
             let zoomLevel = this.leaflet.map.getZoom();
-            if (zoomLevel <= 13){
+            if (zoomLevel <= 13) {
                 zoomLevel = 13
             }
-            this.leaflet.map.setView(e.latlng,zoomLevel);
+            this.leaflet.map.setView(e.latlng, zoomLevel);
         },
 
         onMapMove(e) {
@@ -127,7 +130,7 @@ export default {
         },
 
         updateTable(filter) {
-            if (filter.removed == true && this.currFilter != null){
+            if (filter.removed == true && this.currFilter != null) {
                 url = this.currFilter;
             } else {
                 var url = 'http://localhost:8080/incidents?';
@@ -168,28 +171,45 @@ export default {
             });
         },
 
-        getViewableNeighborhoods(){
+        getViewableNeighborhoods() {
             var corners = this.leaflet.map.getBounds();
             var ne = corners.getNorthEast();
             var sw = corners.getSouthWest();
             console.log(`NE: ${ne}, SW:${sw}`);
             let inRange = []
-            for (var i =0;i<this.leaflet.neighborhood_markers.length;i++){
-                if ((parseFloat(sw.lat) <= parseFloat(this.leaflet.neighborhood_markers[i].location[0]) <= parseFloat(ne.lat)) && (parseFloat(sw.lng) <= this.leaflet.neighborhood_markers[i].location[1]) && (this.leaflet.neighborhood_markers[i].location[1] <= parseFloat(ne.lng))){
+            for (var i = 0; i < this.leaflet.neighborhood_markers.length; i++) {
+                if ((parseFloat(sw.lat) <= parseFloat(this.leaflet.neighborhood_markers[i].location[0]) <= parseFloat(ne.lat)) && (parseFloat(sw.lng) <= this.leaflet.neighborhood_markers[i].location[1]) && (this.leaflet.neighborhood_markers[i].location[1] <= parseFloat(ne.lng))) {
                     inRange.push(this.leaflet.neighborhood_markers[i].marker);
                 }
             }
             console.log(inRange);
-            var greenIcon = new L.Icon({
-  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
-  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  shadowSize: [41, 41]
-            });
-            var marker = L.marker([ne.lat,ne.lng],{icon:greenIcon}).addTo(this.leaflet.map)
-            var marker = L.marker([sw.lat,sw.lng],{icon:greenIcon}).addTo(this.leaflet.map)
+            // var marker = L.marker([ne.lat,ne.lng],{icon:greenIcon}).addTo(this.leaflet.map)
+            // var marker = L.marker([sw.lat,sw.lng],{icon:greenIcon}).addTo(this.leaflet.map)
+        },
+
+        toggleMarker(incident) {
+            const oldMarker = this.visibleMarkers.filter(d => d.case == incident.case_number)[0];
+            if (oldMarker) {
+                oldMarker.marker.remove(this.leaflet.map);
+                this.visibleMarkers = this.visibleMarkers.filter(d => d != oldMarker);
+            } else {
+                let loc = incident.block.split(" ");
+                loc[0] = loc[0].replaceAll(/X/g, '0');
+                loc.push("MN");
+                loc.push("SAINT PAUL");
+                loc = loc.join("+");
+                this.getText(`https://nominatim.openstreetmap.org/search?q=${loc}&format=jsonv2`).then(result => {
+                    const parsed = JSON.parse(result);
+                    if (parsed.length <= 0) return;
+                    const location = parsed[0];
+                    const marker = L.marker([location.lat, location.lon], { icon: this.leaflet.icons.green });
+                    this.visibleMarkers.push({ case: incident.case_number, marker: marker});
+                    marker.bindPopup(`Date: ${incident.date}\r\n Time: ${incident.time}, Incident: ${incident.incident}, DELETE BUTTON HERE`);
+                    marker.addTo(this.leaflet.map);
+                }).catch((err) => {
+                    console.log(err);
+                })
+            }
         },
 
         getText(url) {
@@ -239,95 +259,95 @@ export default {
                 });
             });
         },
-        placeMarkers(layerGroup){
+        placeMarkers(layerGroup) {
             this.getJSON('http://localhost:8080/neighborhoods').then((result) => {
-            this.neighborhoods = result;
-            if (this.incidents.length==0){
-                this.getJSON('http://localhost:8080/incidents?limit=1000').then((result) => {
+                this.neighborhoods = result;
+                if (this.incidents.length == 0) {
+                    this.getJSON('http://localhost:8080/incidents?limit=1000').then((result) => {
                         this.incidents = result;
                         for (var i = 0; i < this.leaflet.neighborhood_markers.length; i++) {
-                        var location = this.leaflet.neighborhood_markers[i].location;
+                            var location = this.leaflet.neighborhood_markers[i].location;
+                            var name = this.leaflet.neighborhood_markers[i].marker;
+                            var marker = L.marker(location, { title: name }).on('click', this.onMapClick);
+                            var incidentNumber = 0;
+                            for (var j = 0; j < this.incidents.length; j++) {
+                                if (this.incidents[j].neighborhood_number == i + 1) {
+                                    incidentNumber++;
+                                }
+                            }
+                            marker.bindPopup(`Incidents in the ${name} Neighborhood: ${incidentNumber}`);
+                            layerGroup.addLayer(marker);
+                        }
+                        layerGroup.addTo(this.leaflet.map);
+                    })
+                        .catch((error) => {
+                            console.log(error);
+                        });
+                } else {
+                    for (var i = 0; i < this.leaflet.neighborhood_markers.length; i++) {
                         var name = this.leaflet.neighborhood_markers[i].marker;
-                        var marker = L.marker(location, {title:name}).on('click', this.onMapClick);
                         var incidentNumber = 0;
-                        for (var j=0;j<this.incidents.length;j++) {
-                            if (this.incidents[j].neighborhood_number == i+1) {
+                        for (var j = 0; j < this.incidents.length; j++) {
+                            if (this.incidents[j].neighborhood_number == i + 1) {
                                 incidentNumber++;
                             }
                         }
-                        marker.bindPopup(`Incidents in the ${name} Neighborhood: ${incidentNumber}`);
-                        layerGroup.addLayer(marker);
+                        layerGroup.eachLayer(function (layer) {
+                            if (layer.options.title === name) {
+                                layer.bindPopup(`Incidents in the ${name} Neighborhood: ${incidentNumber}`);
+                            }
+                        })
                     }
-                    layerGroup.addTo(this.leaflet.map);
-                })
-                .catch((error) => {
-                    console.log(error);
-                });  
-            } else {
-                for (var i = 0; i < this.leaflet.neighborhood_markers.length; i++) {
-                    var name = this.leaflet.neighborhood_markers[i].marker;
-                    var incidentNumber = 0;
-                    for (var j=0;j<this.incidents.length;j++) {
-                        if (this.incidents[j].neighborhood_number == i+1) {
-                            incidentNumber++;
-                        }
-                    }
-                    layerGroup.eachLayer(function (layer) {
-                        if(layer.options.title === name){
-                            layer.bindPopup(`Incidents in the ${name} Neighborhood: ${incidentNumber}`);
-                        }
-                    })
-                }
                 }
             }).catch((error) => {
                 console.log(error);
-            }); 
+            });
         },
-        getRowColor(incident){
+        getRowColor(incident) {
             let code = incident.code
-            let personal = [100, 110, 120, 210, 220,400, 410, 411, 412, 420, 421, 422, 430, 431, 432, 440, 441, 442, 450, 451, 452, 453,810, 861, 862, 863];
+            let personal = [100, 110, 120, 210, 220, 400, 410, 411, 412, 420, 421, 422, 430, 431, 432, 440, 441, 442, 450, 451, 452, 453, 810, 861, 862, 863];
             let property = [300, 311, 312, 313, 314, 321, 322, 323, 324, 331, 333, 334, 341, 342, 343, 344, 351, 352, 353, 354, 361, 363, 364, 371, 372, 373
-            , 374, 500, 510, 511, 513, 515, 516, 520, 521, 523, 525, 526, 530, 531, 533, 535, 536, 540, 541, 543, 545, 546, 550, 551, 553, 555, 556, 560, 561
-            , 563, 565, 566, 600, 603, 611, 612, 613, 614, 621, 622, 623, 630, 631, 632, 633, 640, 641, 642, 643, 651, 652,653, 661, 662, 663,671, 672, 673
-            , 681, 682, 683, 691,692, 693, 700, 710, 711, 712, 720,721, 722, 730,731, 732, 900, 901, 903, 905, 911, 913, 915, 921, 922, 923, 925, 931, 933, 941
-            , 942, 951, 961, 971, 972, 975, 981, 982, 1400, 1401, 1410, 1415, 1416, 1420, 1425, 1426, 1430, 1435, 1436];
-            if (personal.includes(code)){
+                , 374, 500, 510, 511, 513, 515, 516, 520, 521, 523, 525, 526, 530, 531, 533, 535, 536, 540, 541, 543, 545, 546, 550, 551, 553, 555, 556, 560, 561
+                , 563, 565, 566, 600, 603, 611, 612, 613, 614, 621, 622, 623, 630, 631, 632, 633, 640, 641, 642, 643, 651, 652, 653, 661, 662, 663, 671, 672, 673
+                , 681, 682, 683, 691, 692, 693, 700, 710, 711, 712, 720, 721, 722, 730, 731, 732, 900, 901, 903, 905, 911, 913, 915, 921, 922, 923, 925, 931, 933, 941
+                , 942, 951, 961, 971, 972, 975, 981, 982, 1400, 1401, 1410, 1415, 1416, 1420, 1425, 1426, 1430, 1435, 1436];
+            if (personal.includes(code)) {
                 return 'background:#FB8072';
-            } else if (property.includes(code)){
+            } else if (property.includes(code)) {
                 return 'background:#FFFFB3'
             } else {
                 return "background:#80B1D3";
             }
         },
-        deleteIncident(incident){
+        deleteIncident(incident) {
             console.log('deleting instance');
             console.log(incident);
             console.log(incident.case_number);
-            let json_data = JSON.stringify({"case_number":incident.case_number});
+            let json_data = JSON.stringify({ "case_number": incident.case_number });
             console.log(json_data);
-                $.ajax({
-                    type: 'DELETE',
-                    contentType: 'application/json',
-                    url: 'http://localhost:8080/remove-incident',
-                    data:json_data,
-                    success: (response) => {
-                        alert("Successfully removed incident from the Database!");
-                        this.updateTable({
-                            incidents: [],
-                            neighborhoods: [],
-                            startTime: '',
-                            endTime: '',
-                            startDate: '',
-                            endDate: '',
-                            limit: 1000,
-                            removed: true
-                        });
-                    },
-                    error: (status, message) => {
-                        console.log({ status: status.status, message: status.statusText });
-                    }
-                });
-        
+            $.ajax({
+                type: 'DELETE',
+                contentType: 'application/json',
+                url: 'http://localhost:8080/remove-incident',
+                data: json_data,
+                success: (response) => {
+                    alert("Successfully removed incident from the Database!");
+                    this.updateTable({
+                        incidents: [],
+                        neighborhoods: [],
+                        startTime: '',
+                        endTime: '',
+                        startDate: '',
+                        endDate: '',
+                        limit: 1000,
+                        removed: true
+                    });
+                },
+                error: (status, message) => {
+                    console.log({ status: status.status, message: status.statusText });
+                }
+            });
+
 
         }
     },
@@ -345,6 +365,15 @@ export default {
         this.leaflet.map.on('moveend', this.onMapMove);
         let district_boundary = new L.geoJson();
         district_boundary.addTo(this.leaflet.map);
+
+        this.leaflet.icons.green = new L.Icon({
+            iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+            shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+            iconSize: [25, 41],
+            iconAnchor: [12, 41],
+            popupAnchor: [1, -34],
+            shadowSize: [41, 41]
+        });
 
         this.getJSON('/data/StPaulDistrictCouncil.geojson').then((result) => {
             // St. Paul GeoJSON
@@ -389,10 +418,10 @@ export default {
             <div v-show="filter === true">
                 <FilterForm @update-rows="updateTable" />
             </div>
-        </div>
+        </div>        
         <div class='my-legend'>
-            <div class='legend-title'>Different Crime Categories</div>
             <div class='legend-scale'>
+                <p class='legend-title'>Crime Categories:</p>
                 <ul class='legend-labels'>
                     <li><span style='background:#FB8072;'></span>Violent Crimes</li>
                     <li><span style='background:#FFFFB3;'></span>Property Crimes</li>
@@ -400,9 +429,9 @@ export default {
                 </ul>
             </div>
         </div>
-        <div>
+        <div style="overflow-y: scroll; height:333px;">
             <table class="table">
-                <thead>
+                <thead style="position: sticky; top: -1px">
                     <tr>
                         <th>Case Number</th>
                         <th>Date</th>
@@ -411,7 +440,8 @@ export default {
                         <th>Police Grid</th>
                         <th>Neighborhood</th>
                         <th>Block</th>
-                        <th></th>
+                        <th>Show</th>
+                        <th>Delete</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -423,10 +453,14 @@ export default {
                         <td>{{ incident.police_grid }}</td>
                         <td>{{ neighborhoods.filter(n => n.ID == incident.neighborhood_number)[0].Name }}</td>
                         <td>{{ incident.block }}</td>
-                        <td><button @click="deleteIncident(incident)"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-  <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
-  <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
-</svg></button></td>
+                        <td><input type="checkbox" @change="toggleMarker(incident)" /></td>
+                        <td><button @click="deleteIncident(incident)"><svg xmlns="http://www.w3.org/2000/svg" width="16"
+                                    height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                                    <path
+                                        d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
+                                    <path fill-rule="evenodd"
+                                        d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
+                                </svg></button></td>
                     </tr>
                 </tbody>
             </table>
@@ -464,30 +498,39 @@ export default {
 }
 
 .my-legend {
-    padding-left: 5px;
+    display:inline-block;
+    margin-left: 10px;
 }
 
 .my-legend .legend-title {
     text-align: left;
+    display: inline;
+    float: left;
+    padding-right: 5px;
     margin-bottom: 5px;
     font-weight: bold;
     font-size: 90%;
-    }
-  .my-legend .legend-scale ul {
+}
+
+.my-legend .legend-scale ul {
     margin: 0;
     margin-bottom: 5px;
     padding: 0;
     float: left;
     list-style: none;
-    }
-  .my-legend .legend-scale ul li {
+}
+
+.my-legend .legend-scale ul li {
+    display: inline-block;
     font-size: 80%;
     list-style: none;
     margin-left: 0;
+    padding-right: 5px;
     line-height: 18px;
     margin-bottom: 2px;
-    }
-  .my-legend ul.legend-labels li span {
+}
+
+.my-legend ul.legend-labels li span {
     display: block;
     float: left;
     height: 16px;
@@ -495,8 +538,9 @@ export default {
     margin-right: 5px;
     margin-left: 0;
     border: 1px solid #999;
-    }
-  .my-legend a {
+}
+
+.my-legend a {
     color: #777;
-    }
+}
 </style>
